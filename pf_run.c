566c3d3d19b8b532ae7fcb1d4a920f54ea7a86c2
Bart Trojanowski <bart@jukie.net>
2005-11-21 22:03:00 -0500

    changed some structure names

--- pf_run.c
+++ pf_run.c
@@ -52,18 +52,18 @@ typedef struct {
         // what is completed
         uint            no_failed;
         uint            no_completed;
-} pf_state_t;
+} pf_run_t;
 
 // ------------------------------------------------------------------------
 
-static int pf_state_init (pf_state_t *state, const pf_conf_t *conf);
-static void pf_state_cleanup (pf_state_t *state);
-static int pf_state_open_sockets (pf_state_t *state);
-static int pf_state_create_connections (pf_state_t *state);
-static int pf_state_prepare_for_io (pf_state_t *state);
-static int pf_state_perform_select (pf_state_t *state);
-static int pf_state_perform_io (pf_state_t *state);
-static void pf_state_display (pf_state_t *state, int force);
+static int pf_run_init (pf_run_t *run, const pf_conf_t *conf);
+static void pf_run_cleanup (pf_run_t *run);
+static int pf_run_open_sockets (pf_run_t *run);
+static int pf_run_create_connections (pf_run_t *run);
+static int pf_run_prepare_for_io (pf_run_t *run);
+static int pf_run_perform_select (pf_run_t *run);
+static int pf_run_perform_io (pf_run_t *run);
+static void pf_run_display (pf_run_t *run, int force);
 
 // ------------------------------------------------------------------------
 
@@ -71,9 +71,9 @@ int
 pf_run (const pf_conf_t *conf)
 {
         int rc;
-        pf_state_t state;
+        pf_run_t run;
 
-        rc = pf_state_init (&state, conf);
+        rc = pf_run_init (&run, conf);
         if (rc<0) {
                 DBG (1, "failed to init state structure, rc=%d\n", rc);
                 return rc;
@@ -81,35 +81,35 @@ pf_run (const pf_conf_t *conf)
 
         DBG (1, "main loop\n");
         // main loop
-        while (state.no_completed < conf->no_connections) {
+        while (run.no_completed < conf->no_connections) {
 
                 DBG (1, "\n------------------------------------------------------------\n");
-                pf_state_display (&state, 0);
+                pf_run_display (&run, 0);
                 DBG (1, "\n");
 
                 // open new sockets
-                rc = pf_state_open_sockets (&state);
+                rc = pf_run_open_sockets (&run);
                 if (rc<0) {
                         DBG (1, "failed to open sockets, rc=%d\n", rc);
                         return rc;
                 }
 
                 // create connections
-                rc = pf_state_create_connections (&state);
+                rc = pf_run_create_connections (&run);
                 if (rc<0) {
                         DBG (1, "failed to open sockets, rc=%d\n", rc);
                         return rc;
                 }
 
                 // prepare the select bits
-                rc = pf_state_prepare_for_io (&state);
+                rc = pf_run_prepare_for_io (&run);
                 if (rc<0) {
                         DBG (1, "failed to prepare for IO, rc=%d\n", rc);
                         return rc;
                 }
 
                 // wait for IO to become available
-                rc = pf_state_perform_select (&state);
+                rc = pf_run_perform_select (&run);
                 if (rc<0) {
                         if (errno == EINTR)
                                 continue;
@@ -119,17 +119,17 @@ pf_run (const pf_conf_t *conf)
                 }
 
                 // do the IO operations
-                rc = pf_state_perform_io (&state);
+                rc = pf_run_perform_io (&run);
                 if (rc<0) {
                         DBG (1, "failed to perform IO operations, rc=%d\n", rc);
                         return rc;
                 }
 
         }
-        pf_state_display (&state, 1);
+        pf_run_display (&run, 1);
         DBG (0, "\n");
 
-        pf_state_cleanup (&state);
+        pf_run_cleanup (&run);
 
         return 0;
 }
@@ -137,121 +137,121 @@ pf_run (const pf_conf_t *conf)
 // ------------------------------------------------------------------------
 
 static int 
-pf_state_init (pf_state_t *s, const pf_conf_t *conf)
+pf_run_init (pf_run_t *r, const pf_conf_t *conf)
 {
         uint i;
 
-        memset (s, 0, sizeof (*s));
-        s->conf = conf;
+        memset (r, 0, sizeof (*r));
+        r->conf = conf;
 
         // timing
-        gettimeofday (&s->start_time, NULL);
+        gettimeofday (&r->start_time, NULL);
 
         // display config
-        s->next_update  = s->start_time;
-        s->update_delta = (struct timeval) {0,10000};
+        r->next_update  = r->start_time;
+        r->update_delta = (struct timeval) {0,10000};
 
         // allocate agents
-        s->agents = calloc (conf->no_agents, sizeof (pf_ctx_t));
-        if (!s->agents) BAIL ("failed to allocate array");
+        r->agents = calloc (conf->no_agents, sizeof (pf_ctx_t));
+        if (!r->agents) BAIL ("failed to allocate array");
 
         // allocate state masks
-        s->mask_long_cnt = 2 + (conf->no_agents/(sizeof(ulong) * 8));
+        r->mask_long_cnt = 2 + (conf->no_agents/(sizeof(ulong) * 8));
 
-        s->ctx_has_sock_mask = calloc (s->mask_long_cnt, sizeof (ulong));
-        if (!s->ctx_has_sock_mask) BAIL ("failed to allocate array");
+        r->ctx_has_sock_mask = calloc (r->mask_long_cnt, sizeof (ulong));
+        if (!r->ctx_has_sock_mask) BAIL ("failed to allocate array");
 
-        s->ctx_need_conn_mask = calloc (s->mask_long_cnt, sizeof (ulong));
-        if (!s->ctx_need_conn_mask) BAIL ("failed to allocate array");
+        r->ctx_need_conn_mask = calloc (r->mask_long_cnt, sizeof (ulong));
+        if (!r->ctx_need_conn_mask) BAIL ("failed to allocate array");
 
-        s->ctx_did_conn_mask = calloc (s->mask_long_cnt, sizeof (ulong));
-        if (!s->ctx_did_conn_mask) BAIL ("failed to allocate array");
+        r->ctx_did_conn_mask = calloc (r->mask_long_cnt, sizeof (ulong));
+        if (!r->ctx_did_conn_mask) BAIL ("failed to allocate array");
 
         // initialize
         DBG (1, "initialzie contexts\n");
         for (i=0; i<conf->no_agents; i++)
-                pf_ctx_init (&s->agents[i], conf);
+                pf_ctx_init (&r->agents[i], conf);
 
         return 0;
 }
 
 static void
-pf_state_cleanup (pf_state_t *s)
+pf_run_cleanup (pf_run_t *r)
 {
-        free(s->ctx_has_sock_mask);
-        free(s->ctx_need_conn_mask);
-        free(s->ctx_did_conn_mask);
-        free (s->agents);
+        free(r->ctx_has_sock_mask);
+        free(r->ctx_need_conn_mask);
+        free(r->ctx_did_conn_mask);
+        free (r->agents);
 }
 
 static int 
-pf_state_open_sockets (pf_state_t *s)
+pf_run_open_sockets (pf_run_t *r)
 {
         int rc;
         uint i;
-        const pf_conf_t *conf = s->conf;
+        const pf_conf_t *conf = r->conf;
 
         DBG (2, "\n - open sockets\n");
-        while (s->ctx_has_sock_count < conf->no_agents) {
+        while (r->ctx_has_sock_count < conf->no_agents) {
 
                 pf_ctx_t *ctx;
 
                 DBG (2, "  ctx_has_sock_mask: ");
-                for (i=0; i<s->mask_long_cnt; i++)
-                        DBG (2, "%08lx ", s->ctx_has_sock_mask[i]);
+                for (i=0; i<r->mask_long_cnt; i++)
+                        DBG (2, "%08lx ", r->ctx_has_sock_mask[i]);
                 DBG (2, "\n");
 
                 // find one that is not being used
-                i = find_first_zero_bit (s->ctx_has_sock_mask,
+                i = find_first_zero_bit (r->ctx_has_sock_mask,
                                 conf->no_agents);
                 if (i >= conf->no_agents)
                         BAIL ("failed to find free agent (cnt=%d/%d)",
-                                        s->ctx_has_sock_count, conf->no_agents);
+                                        r->ctx_has_sock_count, conf->no_agents);
 
                 DBG (2, "  new socket on agent %u/%u\n", i, conf->no_agents);
-                ctx = &s->agents[i];
+                ctx = &r->agents[i];
 
                 // start it up
                 rc = pf_ctx_new (ctx);
                 if (rc<0) break;
 
                 // update state
-                set_bit (i, s->ctx_has_sock_mask);
-                s->ctx_has_sock_count ++;
+                set_bit (i, r->ctx_has_sock_mask);
+                r->ctx_has_sock_count ++;
 
-                set_bit (i, s->ctx_need_conn_mask);
-                s->ctx_need_conn_count ++;
+                set_bit (i, r->ctx_need_conn_mask);
+                r->ctx_need_conn_count ++;
         }
         return 0;
 }
 
 static int 
-pf_state_create_connections (pf_state_t *s)
+pf_run_create_connections (pf_run_t *r)
 {
         int rc;
         uint i;
-        const pf_conf_t *conf = s->conf;
+        const pf_conf_t *conf = r->conf;
 
         DBG (2, "\n - start connections\n");
-        while (s->ctx_need_conn_count) {
+        while (r->ctx_need_conn_count) {
 
                 pf_ctx_t *ctx;
 
                 DBG (2, "  ctx_need_conn_mask: ");
-                for (i=0; i<s->mask_long_cnt; i++)
-                        DBG (2, "%08lx ", s->ctx_need_conn_mask[i]);
+                for (i=0; i<r->mask_long_cnt; i++)
+                        DBG (2, "%08lx ", r->ctx_need_conn_mask[i]);
                 DBG (2, "\n");
 
                 // find one that is not being used
-                i = find_first_bit (s->ctx_need_conn_mask,
+                i = find_first_bit (r->ctx_need_conn_mask,
                                 conf->no_agents);
                 DBG (2, "  %u\n",i);
                 if (i >= conf->no_agents)
                         BAIL ("failed to find agent for connection (cnt=%d/%d)",
-                                        s->ctx_need_conn_count, conf->no_agents);
+                                        r->ctx_need_conn_count, conf->no_agents);
 
                 DBG (1, "  new connection on agent %u/%u\n", i, conf->no_agents);
-                ctx = &s->agents[i];
+                ctx = &r->agents[i];
 
                 rc = pf_ctx_connect (ctx);
                 if (rc<0) {
@@ -260,67 +260,67 @@ pf_state_create_connections (pf_state_t *s)
                         pf_ctx_close (ctx);
                         pf_ctx_reset (ctx);
 
-                        clear_bit (i, s->ctx_has_sock_mask);
-                        s->ctx_has_sock_count --;
-                        clear_bit (i, s->ctx_need_conn_mask);
-                        s->ctx_need_conn_count --;
+                        clear_bit (i, r->ctx_has_sock_mask);
+                        r->ctx_has_sock_count --;
+                        clear_bit (i, r->ctx_need_conn_mask);
+                        r->ctx_need_conn_count --;
 
-                        s->no_failed ++;
+                        r->no_failed ++;
                         break;
                 }
 
                 conf->do_connected (ctx);
 
                 // update state
-                clear_bit (i, s->ctx_need_conn_mask);
-                s->ctx_need_conn_count --;
+                clear_bit (i, r->ctx_need_conn_mask);
+                r->ctx_need_conn_count --;
 
-                set_bit (i, s->ctx_did_conn_mask);
-                s->ctx_did_conn_count ++;
+                set_bit (i, r->ctx_did_conn_mask);
+                r->ctx_did_conn_count ++;
         }
 
         return 0;
 }
 
 static int 
-pf_state_prepare_for_io (pf_state_t *s)
+pf_run_prepare_for_io (pf_run_t *r)
 {
         uint i;
-        const pf_conf_t *conf = s->conf;
+        const pf_conf_t *conf = r->conf;
 
-        s->max_fd = 0;
-        FD_ZERO (&s->rd_set);
-        FD_ZERO (&s->wr_set);
-        FD_ZERO (&s->er_set);
-        s->rd_cnt = s->wr_cnt = s->er_cnt = 0;
+        r->max_fd = 0;
+        FD_ZERO (&r->rd_set);
+        FD_ZERO (&r->wr_set);
+        FD_ZERO (&r->er_set);
+        r->rd_cnt = r->wr_cnt = r->er_cnt = 0;
 
         // figure out what to select on
         DBG (2, "\n - select selection\n");
         for (i=0; i<conf->no_agents; i++) {
-                pf_ctx_t *ctx = &s->agents[i];
+                pf_ctx_t *ctx = &r->agents[i];
 
                 // only those in connection are eligable
-                if (! test_bit (i, s->ctx_did_conn_mask))
+                if (! test_bit (i, r->ctx_did_conn_mask))
                         continue;
 
                 DBG (2, "  doing IO on %u/%u %s\n", i, conf->no_agents,
                                 (ctx->wants_to_send_more) ? "[W]" : "");
 
-                if (s->max_fd < ctx->fd)
-                        s->max_fd = ctx->fd;
+                if (r->max_fd < ctx->fd)
+                        r->max_fd = ctx->fd;
 
                 // we always want exceptions
-                FD_SET (ctx->fd, &s->er_set);
-                s->er_cnt ++;
+                FD_SET (ctx->fd, &r->er_set);
+                r->er_cnt ++;
 
                 // we always want to read
-                FD_SET (ctx->fd, &s->rd_set);
-                s->rd_cnt ++;
+                FD_SET (ctx->fd, &r->rd_set);
+                r->rd_cnt ++;
 
                 // we sometimes want to write
                 if (ctx->wants_to_send_more) {
-                        FD_SET (ctx->fd, &s->wr_set);
-                        s->wr_cnt ++;
+                        FD_SET (ctx->fd, &r->wr_set);
+                        r->wr_cnt ++;
                 }
         }
 
@@ -328,33 +328,33 @@ pf_state_prepare_for_io (pf_state_t *s)
 }
 
 static int 
-pf_state_perform_select (pf_state_t *s)
+pf_run_perform_select (pf_run_t *r)
 {
         int rc;
 
         DBG (1, "\n - selecting (r=%u, w=%u, e=%u)\n", 
-                        s->rd_cnt, s->wr_cnt, s->er_cnt);
+                        r->rd_cnt, r->wr_cnt, r->er_cnt);
 
         // wait for events
-        rc = select (s->max_fd+1, &s->rd_set, &s->wr_set, &s->er_set, NULL);
+        rc = select (r->max_fd+1, &r->rd_set, &r->wr_set, &r->er_set, NULL);
         DBG (2, "  return %d\n", rc);
 
         return rc;
 }
 
 static int 
-pf_state_perform_io (pf_state_t *s)
+pf_run_perform_io (pf_run_t *r)
 {
         int rc;
         uint i;
-        const pf_conf_t *conf = s->conf;
+        const pf_conf_t *conf = r->conf;
 
         for (i=0; i<conf->no_agents; i++) {
-                pf_ctx_t *ctx = &s->agents[i];
+                pf_ctx_t *ctx = &r->agents[i];
                 int closing = 0;
                 int success = 0;
 
-                if (FD_ISSET (ctx->fd, &s->rd_set)) {
+                if (FD_ISSET (ctx->fd, &r->rd_set)) {
 
                         DBG (2, "  read on %u/%u\n", i, conf->no_agents);
                         rc = conf->do_recv (ctx);
@@ -364,7 +364,7 @@ pf_state_perform_io (pf_state_t *s)
                 }
 
                 if (!closing && ctx->wants_to_send_more 
-                                && FD_ISSET (ctx->fd, &s->wr_set)) {
+                                && FD_ISSET (ctx->fd, &r->wr_set)) {
 
                         DBG (2, "  write on %u/%u\n", i, conf->no_agents);
                         rc = conf->do_send (ctx);
@@ -372,7 +372,7 @@ pf_state_perform_io (pf_state_t *s)
                         if (rc<=0) closing = 1;
                 }
 
-                if (!closing && FD_ISSET (ctx->fd, &s->er_set)) {
+                if (!closing && FD_ISSET (ctx->fd, &r->er_set)) {
 
                         BAIL ("exception on %u/%u\n", i, conf->no_agents);
                         closing = 1;
@@ -384,15 +384,15 @@ pf_state_perform_io (pf_state_t *s)
                         pf_ctx_close (ctx);
                         pf_ctx_reset (ctx);
 
-                        clear_bit (i, s->ctx_has_sock_mask);
-                        s->ctx_has_sock_count --;
-                        clear_bit (i, s->ctx_did_conn_mask);
-                        s->ctx_did_conn_count --;
+                        clear_bit (i, r->ctx_has_sock_mask);
+                        r->ctx_has_sock_count --;
+                        clear_bit (i, r->ctx_did_conn_mask);
+                        r->ctx_did_conn_count --;
 
                         if (success)
-                                s->no_completed ++;
+                                r->no_completed ++;
                         else
-                                s->no_failed ++;
+                                r->no_failed ++;
                 }
         }
 
@@ -400,10 +400,10 @@ pf_state_perform_io (pf_state_t *s)
 }
 
 static void 
-pf_state_display (pf_state_t *s, int force)
+pf_run_display (pf_run_t *r, int force)
 {
         int do_display = force;
-        const pf_conf_t *conf = s->conf;
+        const pf_conf_t *conf = r->conf;
         struct timeval now, diff;
 
         gettimeofday (&now, NULL);
@@ -414,28 +414,28 @@ pf_state_display (pf_state_t *s, int force)
         else if (!force && dbg_level == 0) {
 
                 do_display = 0;
-                if (! timercmp(&s->next_update, &now, >)) {
+                if (! timercmp(&r->next_update, &now, >)) {
                         do_display = 1;
 
-                        timeradd (&now, &s->update_delta, &s->next_update);
+                        timeradd (&now, &r->update_delta, &r->next_update);
                 }
         }
 
         if (do_display) {
                 double us, conn_per_sec;
 
-                timersub (&now, &s->start_time, &diff);
+                timersub (&now, &r->start_time, &diff);
 
                 us = diff.tv_sec + diff.tv_sec/1000000.0;
 
-                conn_per_sec = s->no_completed / us;
+                conn_per_sec = r->no_completed / us;
 
-                printf ("completed %u/%u  %f conn/s  "
+                printf ("completed %u/%u  %f conn/r  "
                         "(sock %u, start %u, conn %u), fail %u           \r", 
-                        s->no_completed, conf->no_connections, 
+                        r->no_completed, conf->no_connections, 
                         conn_per_sec,
-                        s->ctx_has_sock_count, s->ctx_need_conn_count, 
-                        s->ctx_did_conn_count, s->no_failed);
+                        r->ctx_has_sock_count, r->ctx_need_conn_count, 
+                        r->ctx_did_conn_count, r->no_failed);
         }
 }
 
